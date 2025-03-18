d7d004d517bcc28f2d0ac7f003d24cf4bb4ffcef
Elheid <Elfieleintsvard@mail.ru>
2025-03-19 00:44:49 +0500

    refactor

--- src/components/pages/RoundStart.tsx
+++ src/components/pages/RoundStart.tsx
@@ -2,14 +2,25 @@ import { Button } from "@mui/material";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import { endSession } from "../../api/api";
+import axios from "axios";
 
 export const RoundStart = ()=>{
     const nav = useNavigate()
-        useEffect(()=>{
-            endSession()//end prev session
-        },[])
+    useEffect(()=>{
+        endSession()//end prev session
+    },[])
+
+    /**
+     * Just a random request to back, to start a new session on start
+     */
+    const startSessionRequest = ()=>{
+        axios.get("/animals/Олень");
+    }
 
     return (
-        <Button onClick={()=>nav("/first-round")}>Start</Button>
+        <Button onClick={()=>{
+            startSessionRequest()
+            nav("/first-round")
+        }}>Start</Button>
     );
 }
