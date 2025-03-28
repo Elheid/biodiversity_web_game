f69a48a40a50fe02a2496ba81ffaf254b7eac603
Elheid <Elfieleintsvard@mail.ru>
2025-03-28 13:01:18 +0500

    refactor

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,17 @@
-
+//Нужно только для того, чтобы если CURRENT_DURATION_TIME или CURRENT_TIME_BEETWEN_ROUNDS не будет
 export const BASE_TIME_BEETWEN_ROUNDS = 1200;
 export const BASE_DURATION_TIME = 40000;
-
+//Настройки врмени
 export const CURRENT_DURATION_TIME = 40000;//время в милисекундах
 export const CURRENT_TIME_BEETWEN_ROUNDS = 1200;
-
+//
 export const START_ROUND = 0;
 export const START_SCORE = 0;
-
+//Сервер
 const serverIp = "localhost"//"84.201.144.181"//"84.201.144.181"//.168.1.187:5001
 export const SERVER_URL = `http://${serverIp}:5001`//"http://localhost:5001";
 
+//Все слова/тексты
 //MainPage
 export const MAIN_PAGE_TITLE = "Попробуй себя в роли учёного-зоолога!"
 export const MAIN_PAGE_DESCRIPTION = "До 1000 снимков с фотоловушек в день необходимо обрабатывать исследователям особо охраняемых природных территорий"
